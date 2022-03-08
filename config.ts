e46387a21c97b0c7d1c0fa01eb74aa966bab5cbb
dan13ram <dan13ram@gmail.com>
2022-03-08 20:40:44 +0530

    fix: minor fixes

--- config.ts
+++ config.ts
@@ -6,6 +6,6 @@ export const CONFIG = {
   ENVIRONMENT: process.env.ENVIRONMENT,
   MONGODB_URI_PRODUCTION: process.env.MONGODB_URI,
   MONGODB_URI_DEVELOPMENT: process.env.LOCAL_MONGODB_URI,
-  PORT: process.env.PORT || 3000,
+  PORT: process.env.PORT || 5000,
   JWT_SECRET: process.env.JWT_SECRET
 };
